8d2748d5b482fafdd372a3f6afe0ef37f0170e0b
newmanje11 <jevonte_n@hotmail.com>
2018-07-18 14:33:50 -0400

    .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,55 @@
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l',
+    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
 // Creating vaules for the wins, loses and guesses left
 var wins = 0;
-var loses = 0;
-var userGuesses = 9;
+var losses = 0;
 
-// make the it so the computer  knows to guess a letter guess 
 
-var pcGuess = ['a','b','c','d','e','f','g','h','i','j','k','l',
-'m','n','o','p','q','r','s','t','u','v','w','x','y','z']; 
+var guessesLeft = 9;
+var guessesSoFar = []; 
 
-var pcGuess = pcGuess[Math.floor(Math.random() * pcGuess.length)];
+var guessedLetters = letters[Math.floor(Math.random() * letters.length)]
+console.log(guessedLetters);
 
-// make it so that the computer keeps track of only letter being 
+document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); 
+    console.log(userGuess);
 
-// starting the loop for the game 
-
-for (userGuesses > 0; userGuesses - 1) {
-    if (userGuess === pcGuess) {
-        wins++;
-
-    else {
-        userGuesses--;
+    if (guessesSoFar.indexOf(userGuess) < 0 && letters.indexOf(userGuess) >= 0) {
+        guessesSoFar[guessesSoFar.length]=userGuess;
+        guessesLeft--;
     }
+
+    else if (guessedLetters == userGuess) {
+		wins++;
+    	guessesLeft = 9;
+        guessesSoFar = [];
+        guessedLetters = letters[Math.floor(Math.random() * letters.length)];
+
+    }
+
+    else if (guessesLeft == 0) {
+        		losses++;
+        		guessesLeft = 9;
+        		guessesSoFar = [];
+                guessedLetters = letters[Math.floor(Math.random() * letters.length)];
+                
+    }
+
+    var html = 
+            "<p><h1>The Psychic Game</h1></p>" +
+            "<p><h4>Guess what letter I\'m thinking of</h4></p>" + 
+            "<p>Wins: " + wins + "</p>" +
+            "<p>Losses: " + losses + "</p>" +
+            "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" +
+            "<p>Your guesses so far: " + guessesSoFar + "</p>";
+
+            document.querySelector("#game").innerHTML = html;
 }
+
+
+
+
+
+
